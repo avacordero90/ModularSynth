0716881ee1008360ac7be63981f2c85169d78667
Ava Cordero <avacordero90@gmail.com>
2026-03-07 02:14:47 -0800

    Make clean cause we were dirty

--- src/core/audio_pipeline.d
+++
@@ -1,4 +0,0 @@
-audio_pipeline.o: src/core/audio_pipeline.cpp src/core/audio_pipeline.h \
- src/core/../modules/oscillator.h src/core/../modules/../core/wavetable.h \
- src/core/../modules/filter.h src/core/../modules/envelope.h \
- src/core/../core/wavetable_manager.h src/core/../core/wavetable.h
